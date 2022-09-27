21d99a3fdadc9cbfbeaa4a2de7d9d11acb776f05
Gabriele <gabrieleconcas96@gmail.com>
2022-09-27 15:09:53 +0200

    Phone change done

--- src/app/hero/hero-list/hero-list.component.ts
+++ src/app/hero/hero-list/hero-list.component.ts
@@ -1,44 +1,60 @@
 import { KeyValue } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
+interface Image {
+    name: string;
+    url: string;
+}
+
 @Component({
     selector: 'app-hero-list',
     templateUrl: './hero-list.component.html',
     styleUrls: ['./hero-list.component.scss']
 })
 export class HeroListComponent implements OnInit {
-    images: Map<string, string> = new Map<string, string>([
-        [
-            '0Viola scuro',
-            'https://www.apple.com/euro/iphone-14-pro/a/screens_alt/images/overview/colors/gallery_deep_purple__eog5f5067tw2_large_2x.jpg'
-        ],
-        [
-            '1Oro',
-            'https://www.apple.com/euro/iphone-14-pro/a/screens_alt/images/overview/colors/gallery_gold__gctbxs1dpb6m_large_2x.jpg'
-        ],
-        [
-            '2Argento',
-            'https://www.apple.com/euro/iphone-14-pro/a/screens_alt/images/overview/colors/gallery_silver__e5nps2bqzt26_large_2x.jpg'
-        ],
-        [
-            '3Nero siderale',
-            'https://www.apple.com/euro/iphone-14-pro/a/screens_alt/images/overview/colors/gallery_space_black__dnuzmbh1d7ki_large_2x.jpg'
-        ]
-    ]);
+    selectedImage: Image | undefined;
+    prevImage: Image | undefined;
 
-    selectedImage: Map<string, string> = new Map([['0Viola scuro', '']]);
+    videoVisible: boolean = true;
+    imageIsActive: boolean = false;
+
+    images: Image[] = [
+        {
+            name: 'Viola scuro',
+            url: 'https://www.apple.com/euro/iphone-14-pro/a/screens_alt/images/overview/colors/gallery_deep_purple__eog5f5067tw2_large_2x.jpg'
+        },
+        {
+            name: 'Oro',
+            url: 'https://www.apple.com/euro/iphone-14-pro/a/screens_alt/images/overview/colors/gallery_gold__gctbxs1dpb6m_large_2x.jpg'
+        },
+        {
+            name: 'Argento',
+            url: 'https://www.apple.com/euro/iphone-14-pro/a/screens_alt/images/overview/colors/gallery_silver__e5nps2bqzt26_large_2x.jpg'
+        },
+        {
+            name: 'Nero siderale',
+            url: 'https://www.apple.com/euro/iphone-14-pro/a/screens_alt/images/overview/colors/gallery_space_black__dnuzmbh1d7ki_large_2x.jpg'
+        }
+    ];
 
     constructor() {}
 
-    selectImage(image: string) {
-        this.selectedImage = new Map(
-            [...this.images].filter(([k, v]) => k === image)
-        );
+    selectImage(image: Image) {
+        this.prevImage = this.selectedImage;
+        this.selectedImage = image;
+        this.videoVisible = false;
+        this.imageIsActive = false;
 
-        console.log(this.selectedImage.values().next().value);
+        setTimeout(() => {
+            this.imageIsActive = true;
+        }, 100);
+    }
+
+    isSelected(image?: Image): boolean {
+        return this.selectedImage === image;
     }
 
     ngOnInit(): void {
-        console.log(this.selectedImage.values().next().value);
+        this.selectedImage = this.images[0];
     }
 }
