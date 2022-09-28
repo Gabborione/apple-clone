c880254ed3ee1df60a774623dffd27ff7a88ac12
Gabriele <gabrieleconcas96@gmail.com>
2022-09-28 10:52:04 +0200

    sticky bottomHeader

--- src/app/header/bottom-header/bottom-header.component.ts
+++ src/app/header/bottom-header/bottom-header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-bottom-header',
-  templateUrl: './bottom-header.component.html',
-  styleUrls: ['./bottom-header.component.scss']
+    selector: 'app-bottom-header',
+    templateUrl: './bottom-header.component.html',
+    styleUrls: ['./bottom-header.component.scss']
 })
 export class BottomHeaderComponent implements OnInit {
+    isScrolled: boolean = false;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    @HostListener('window:scroll', ['$event'])
+    scrollMovement(event: Event) {
+        if (window.pageYOffset > 10) this.isScrolled = true;
+        else {
+            this.isScrolled = false;
+        }
+    }
 }
